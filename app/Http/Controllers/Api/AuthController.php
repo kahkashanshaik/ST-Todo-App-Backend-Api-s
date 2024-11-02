36be6ade0da512c71dd7b2bb372699018e3b5ed1
kahkashanshaik <kahkashan1507@gmail.com>
2024-11-02 12:40:26 +0530

    added login end point.

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -5,22 +5,55 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Http\Response;
+use Laravel\Sanctum\Sanctum;
 
 class AuthController extends Controller
 {
-    public function register(Request $request){
-        
+    public function register(Request $request)
+    {
+
         $data = $request->validate([
             'name' => 'required',
             'email' => 'required|unique:users',
             'phone' => 'required',
             'address' => 'required',
+            'department_id' => 'required|exists:departments,id',
             'password' => 'required|min:6|max:8',
         ]);
         User::create($data);
         return response()->json([
             "status" => 'success',
-            "message" => 'User created successfully' 
+            "message" => 'User created successfully'
+        ], 200);
+    }
+
+    public function login(Request $request)
+    {
+        // Get Email & password 
+        $credentials = $request->validate([
+            'email' => 'required|exists:users,email',
+            'password' => 'required',
+        ]);
+        // Compare user email ID and password with databse users.
+        // If user is not authenticated
+        if (!Auth::attempt($credentials)) {
+            return response()->json([
+                "status" => 'failure',
+                "message" => 'User not authorized!',
+            ], Response::HTTP_BAD_REQUEST);
+        }
+        // If in case user got authenticated.
+        //  generate token for that login user.
+        $user = User::where('email', $credentials['email'])->get();
+        $token = $request->user()->createToken('auth_token')->plainTextToken;
+        // return response.
+        return response()->json([
+            "status" => 'success',
+            "token" =>  $token,
+            "user" =>  $user,
+            "message" => 'User Logged In successfully',
         ], 200);
     }
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -6,10 +6,11 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
-    use HasFactory, Notifiable;
+    use HasFactory, Notifiable, HasApiTokens;
 
     /**
      * The attributes that are mass assignable.
@@ -22,6 +23,7 @@ class User extends Authenticatable
         'password',
         'phone',
         'address',
+        'department_id',
     ];
 
     /**
@@ -45,5 +47,10 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+
+    public function department()
+    {
+        $this->hasOne('department');
     }
 }
